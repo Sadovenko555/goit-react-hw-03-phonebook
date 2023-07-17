40f8b49f4a8f4ded9cb2be5a78349974d1a12fc2
Ihor <sadovenko555@gmail.com>
2023-07-17 12:27:47 +0300

    add script

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,19 +11,36 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      console.log('renewed contacts');
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   formSubmit = ({ name, number }) => {
     const contact = {
       id: nanoid(),
       name,
       number,
     };
-    this.state.contacts.some(
+
+    const enterContacts = this.state.contacts.some(
       i =>
-        (i.name.toLowerCase() === contact.name.toLowerCase() &&
+        (i.name === contact.name.toLowerCase() &&
           i.number === contact.number) ||
         i.number === contact.number
-    )
-      ? alert(`${name} is already in contacts`)
+    );
+    enterContacts
+      ? alert(`${name} or ${number} is already in contacts`)
       : this.setState(({ contacts }) => ({
           contacts: [contact, ...contacts],
         }));
